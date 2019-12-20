67c56b7b6c2075425f62ddd61a60894697b1de4b
houwf <houwf@jiguang.cn>
2019-12-20 12:02:04 +0800

    46

---
+++ src/main/java/com/hou/leetcode/solution/array/Permutations46Solution.java
@@ -0,0 +1,49 @@
+package com.hou.leetcode.solution.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description
+ * @auther houwf
+ * @create 2019-12-19 12:04
+ */
+public class Permutations46Solution {
+    private List<List<Integer>> res = null;
+    private List<Integer> used = null;
+    private int[] nums = null;
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        res = new ArrayList<>();
+        this.nums = nums;
+        this.used = new ArrayList<>();
+        backtracking(new ArrayList<>());
+        return res;
+    }
+
+    private void backtracking(List<Integer> result) {
+        if (result.size() == nums.length) {
+            res.add(new ArrayList<>(result));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used.contains(nums[i])) {
+                continue;
+            }
+            result.add(nums[i]);
+            used.add(nums[i]);
+            backtracking(result);
+            result.remove(result.size()-1);
+            used.remove(used.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3};
+        Permutations46Solution solution = new Permutations46Solution();
+        List<List<Integer>> res = solution.permute(nums);
+        System.out.println();
+    }
+}
