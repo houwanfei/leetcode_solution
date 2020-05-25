7e60829eb6f9e6f714f7893f430d53a7d6d750c1
houwf <houwf@jiguang.cn>
2020-05-25 18:49:20 +0800

    103

--- src/main/java/com/hou/leetcode/solution/tree/BinaryTreeZigzagLevelOrderTraversal103Solution.java
+++ src/main/java/com/hou/leetcode/solution/tree/BinaryTreeZigzagLevelOrderTraversal103Solution.java
@@ -2,10 +2,7 @@ package com.hou.leetcode.solution.tree;
 
 import com.hou.leetcode.solution.util.TreeBuildUtil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @Description
@@ -14,15 +11,18 @@ import java.util.Stack;
  */
 public class BinaryTreeZigzagLevelOrderTraversal103Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
-        stack1.push(root);
         List<List<Integer>> res = new ArrayList<>();
-        while (!stack1.empty() || !stack2.empty()) {
-            if (!stack1.empty()) {
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        queue.offer(root);
+        while (!queue.isEmpty() || !stack2.empty()) {
+            if (!queue.isEmpty()) {
                 List<Integer> subRes = new ArrayList<>();
-                while (!stack1.empty()) {
-                    TreeNode node = stack1.pop();
+                while (!queue.isEmpty()) {
+                    TreeNode node = queue.poll();
                     subRes.add(node.val);
                     if (node.left != null) {
                         stack2.push(node.left);
@@ -38,10 +38,10 @@ public class BinaryTreeZigzagLevelOrderTraversal103Solution {
                     TreeNode node = stack2.pop();
                     subRes.add(node.val);
                     if (node.left != null) {
-                        stack1.push(node.left);
+                        queue.offer(node.left);
                     }
                     if (node.right != null) {
-                        stack1.push(node.right);
+                        queue.offer(node.right);
                     }
                 }
                 res.add(subRes);
@@ -52,7 +52,7 @@ public class BinaryTreeZigzagLevelOrderTraversal103Solution {
 
     public static void main(String[] args) {
         BinaryTreeZigzagLevelOrderTraversal103Solution solution = new BinaryTreeZigzagLevelOrderTraversal103Solution();
-        TreeNode head = TreeBuildUtil.buildTreeFromArray(new Integer[]{3, 9, 20, null, null, 15, 7});
+        TreeNode head = TreeBuildUtil.buildTreeFromArray(new Integer[]{1,2,3,4,null,null,5});
         List<List<Integer>> res = solution.zigzagLevelOrder(head);
         System.out.println();
     }
