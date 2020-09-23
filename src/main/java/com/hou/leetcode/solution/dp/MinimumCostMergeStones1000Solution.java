53895a61f14d04828d886b399328204c6d45567f
houwf <houwf@jiguang.cn>
2020-09-23 16:06:35 +0800

    1000 no ac

---
+++ src/main/java/com/hou/leetcode/solution/dp/MinimumCostMergeStones1000Solution.java
@@ -0,0 +1,19 @@
+package com.hou.leetcode.solution.dp;
+
+/**
+ * @Description
+ * @auther houwf
+ * @create 2020-09-23 15:58
+ */
+public class MinimumCostMergeStones1000Solution {
+    public int mergeStones(int[] stones, int K) {
+        return 0;
+    }
+
+    private int helper(int[] stones, int K, int i, int j) {
+        if (j-i == K) {
+            return -1;
+        }
+        return -1;
+    }
+}
